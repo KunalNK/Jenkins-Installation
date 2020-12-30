631e08e4c0bfde82f33dbd6de69d3a224077d893
Kunal K <knkillekar@gmail.com>
2020-12-30 20:57:49 +0530

    script jenkins

---
+++ jenkins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash   
+          
+sudo yum update -y
+sudo yum install java-1.8.0-openjdk
+sudo yum install wget
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo rpm — import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins -y
+sudo service jenkins start
