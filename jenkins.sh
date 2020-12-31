3a339d17fe74083136dfedd22d7ba1c2e74c78b2
Kunal07 <50284603+KunalNK@users.noreply.github.com>
2020-12-31 10:38:07 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash   
      
 sudo yum update -y
-sudo yum install java-1.8.0-openjdk
-sudo yum install wget
+sudo yum install java-1.8.0-openjdk -y
+sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo rpm — import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install jenkins -y
