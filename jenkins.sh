992be30c729c67ec9de9a7e163bbbe6b09d774eb
Kunal07 <50284603+KunalNK@users.noreply.github.com>
2021-01-05 14:25:18 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash   
 sudo yum install wget -y
+sudo yum install git -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat/jenkins.repo 
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
